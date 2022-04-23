3b62137f75e7e22a8359dfc3a6d02694d7adf241
tk-yoshimura <yoshimura8192@gmail.com>
2022-04-24 02:12:19 +0900

    update fig

--- SIMDSort/_main.cpp
+++ SIMDSort/_main.cpp
@@ -9,6 +9,17 @@ int main() {
     sort_reverse_speed_test_s();
     sort_inbalance_speed_test_s();
 
+    //for (uint n = 32; n <= 0x8000000u; n *= 2) {
+    //    printf("%d : ", n);
+    //
+    //    for (uint h = (uint)(n * 10L / 13L); h > 1; h = (uint)(h * 10L / 13L)) {
+    //        printf("%d -> ", h);
+    //    }
+    //
+    //    printf("\n");
+    //}
+
+
     printf("end");
     return getchar();
 }
