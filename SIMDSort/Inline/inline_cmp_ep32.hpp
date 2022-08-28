f8bd143bab05ca756bd1e234a579e0076e1f0e60
tk-yoshimura <yoshimura8192@gmail.com>
2022-08-28 22:49:15 +0900

    +cmp ep32

--- SIMDSort/Inline/inline_cmp_ep32.hpp
+++ SIMDSort/Inline/inline_cmp_ep32.hpp
@@ -33,6 +33,42 @@ __forceinline __m256i _mm256_cmplt_epu32(__m256i x, __m256i y) {
 
 #pragma endregion less than (x < y)
 
+#pragma region greater than or equal (x >= y)
+
+// compare x >= y
+__forceinline __m256i _mm256_cmpge_epi32(__m256i x, __m256i y) {
+    __m256i ge = _mm256_cmpeq_epi32(_mm256_max_epi32(x, y), x);
+
+    return ge;
+}
+
+// compare x >= y
+__forceinline __m256i _mm256_cmpge_epu32(__m256i x, __m256i y) {
+    __m256i ge = _mm256_cmpeq_epi32(_mm256_max_epu32(x, y), x);
+
+    return ge;
+}
+
+#pragma endregion greater than or equal (x >= y)
+
+#pragma region less than (x <= y)
+
+// compare x <= y
+__forceinline __m256i _mm256_cmple_epi32(__m256i x, __m256i y) {
+    __m256i le = _mm256_cmpge_epi32(y, x);
+
+    return le;
+}
+
+// compare x <= y
+__forceinline __m256i _mm256_cmple_epu32(__m256i x, __m256i y) {
+    __m256i le = _mm256_cmpge_epu32(y, x);
+
+    return le;
+}
+
+#pragma endregion less than (x <= y)
+
 #pragma region equals
 
 // compare x == y
--- SIMDSort/_main.cpp
+++ SIMDSort/_main.cpp
@@ -7,21 +7,7 @@
 #include "Inline/inline_cmp_ep32.hpp"
 
 int main() {
-    __m256i a = _mm256_setr_epi32(-2147483648, -2147483647, -256, -1, 0, 1, 255, 2147483647);
-    __m256i b = _mm256_setr_epi32(-2147483648, -2147483647, -256, -1, 0, 1, 255, 2147483647);
-    __m256i c = _mm256_setr_epi32(0, 1, 255, 2147483647, -2147483648, -2147483647, -256, -1);
-    __m256i d = _mm256_setr_epi32(-256, -1, 0, 1, 255, -2147483647, 2147483647, -2147483648);
-    __m256i e = _mm256_setr_epi32(1, -2147483647, 2147483647, -2147483648, 255, -256, -1, 0);
-
-    __m256i ab = _mm256_cmpgt_epu32(a, b);
-    __m256i ac = _mm256_cmpgt_epu32(a, c);
-    __m256i ad = _mm256_cmpgt_epu32(a, d);
-    __m256i ae = _mm256_cmpgt_epu32(a, e);
-
-    __m256i ba = _mm256_cmpgt_epu32(b, a);
-    __m256i ca = _mm256_cmpgt_epu32(c, a);
-    __m256i da = _mm256_cmpgt_epu32(d, a);
-    __m256i ea = _mm256_cmpgt_epu32(e, a);
+    printf("uint32\n");
 
     for (int j = 0; j < 32; j++) {
         for (int i = 0; i < 32; i++) {
@@ -39,24 +25,168 @@ int main() {
             __m256i y2 = _mm256_set1_epi32(e);
             __m256i z2 = _mm256_set1_epi32(f);
 
-            __m256i x12 = _mm256_cmplt_epu32(x1, x2);
-            __m256i y12 = _mm256_cmplt_epu32(y1, y2);
-            __m256i z12 = _mm256_cmplt_epu32(z1, z2);
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmpge_epu32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmpge_epu32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmpge_epu32(z1, z2)) != 0;
 
-            bool xr12 = _mm256_movemask_epi8(x12) != 0;
-            bool yr12 = _mm256_movemask_epi8(y12) != 0;
-            bool zr12 = _mm256_movemask_epi8(z12) != 0;
- 
-            if ((a < d) != xr12) {
-                printf("%u > %u\n", a, d);
+                if ((a >= d) != xr12) {
+                    printf("%u >= %u\n", a, d);
+                }
+
+                if ((b >= e) != yr12) {
+                    printf("%u >= %u\n", b, e);
+                }
+
+                if ((c >= f) != zr12) {
+                    printf("%u >= %u\n", c, f);
+                }
             }
 
-            if ((b < e) != yr12) {
-                printf("%u > %u\n", b, e);
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmple_epu32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmple_epu32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmple_epu32(z1, z2)) != 0;
+
+                if ((a <= d) != xr12) {
+                    printf("%u <= %u\n", a, d);
+                }
+
+                if ((b <= e) != yr12) {
+                    printf("%u <= %u\n", b, e);
+                }
+
+                if ((c <= f) != zr12) {
+                    printf("%u <= %u\n", c, f);
+                }
             }
 
-            if ((c < f) != zr12) {
-                printf("%u > %u\n", c, f);
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmpgt_epu32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmpgt_epu32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmpgt_epu32(z1, z2)) != 0;
+
+                if ((a > d) != xr12) {
+                    printf("%u > %u\n", a, d);
+                }
+
+                if ((b > e) != yr12) {
+                    printf("%u > %u\n", b, e);
+                }
+
+                if ((c > f) != zr12) {
+                    printf("%u > %u\n", c, f);
+                }
+            }
+
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmplt_epu32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmplt_epu32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmplt_epu32(z1, z2)) != 0;
+
+                if ((a < d) != xr12) {
+                    printf("%u < %u\n", a, d);
+                }
+
+                if ((b < e) != yr12) {
+                    printf("%u < %u\n", b, e);
+                }
+
+                if ((c < f) != zr12) {
+                    printf("%u < %u\n", c, f);
+                }
+            }
+        }
+    }
+
+    printf("int32\n");
+
+    for (int j = 0; j < 32; j++) {
+        for (int i = 0; i < 32; i++) {
+            __int32 a = (1 << i) - 1;
+            __int32 b = (1 << i);
+            __int32 c = (1 << i) + 1;
+            __int32 d = (1 << j) - 1;
+            __int32 e = (1 << j);
+            __int32 f = (1 << j) + 1;
+
+            __m256i x1 = _mm256_set1_epi32(a);
+            __m256i y1 = _mm256_set1_epi32(b);
+            __m256i z1 = _mm256_set1_epi32(c);
+            __m256i x2 = _mm256_set1_epi32(d);
+            __m256i y2 = _mm256_set1_epi32(e);
+            __m256i z2 = _mm256_set1_epi32(f);
+
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmpge_epi32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmpge_epi32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmpge_epi32(z1, z2)) != 0;
+
+                if ((a >= d) != xr12) {
+                    printf("%u >= %u\n", a, d);
+                }
+
+                if ((b >= e) != yr12) {
+                    printf("%u >= %u\n", b, e);
+                }
+
+                if ((c >= f) != zr12) {
+                    printf("%u >= %u\n", c, f);
+                }
+            }
+
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmple_epi32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmple_epi32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmple_epi32(z1, z2)) != 0;
+
+                if ((a <= d) != xr12) {
+                    printf("%u <= %u\n", a, d);
+                }
+
+                if ((b <= e) != yr12) {
+                    printf("%u <= %u\n", b, e);
+                }
+
+                if ((c <= f) != zr12) {
+                    printf("%u <= %u\n", c, f);
+                }
+            }
+
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmpgt_epi32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmpgt_epi32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmpgt_epi32(z1, z2)) != 0;
+
+                if ((a > d) != xr12) {
+                    printf("%u > %u\n", a, d);
+                }
+
+                if ((b > e) != yr12) {
+                    printf("%u > %u\n", b, e);
+                }
+
+                if ((c > f) != zr12) {
+                    printf("%u > %u\n", c, f);
+                }
+            }
+
+            {
+                bool xr12 = _mm256_movemask_epi8(_mm256_cmplt_epi32(x1, x2)) != 0;
+                bool yr12 = _mm256_movemask_epi8(_mm256_cmplt_epi32(y1, y2)) != 0;
+                bool zr12 = _mm256_movemask_epi8(_mm256_cmplt_epi32(z1, z2)) != 0;
+
+                if ((a < d) != xr12) {
+                    printf("%u < %u\n", a, d);
+                }
+
+                if ((b < e) != yr12) {
+                    printf("%u < %u\n", b, e);
+                }
+
+                if ((c < f) != zr12) {
+                    printf("%u < %u\n", c, f);
+                }
             }
         }
     }
